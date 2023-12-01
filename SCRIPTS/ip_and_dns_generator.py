e4814e9b9a231ab5f2a693fb27e9a8797a140dbe
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2023-12-01 09:16:21 -0300

    att erros corrigidos

--- SCRIPTS/ip_and_dns_generator.py
+++ SCRIPTS/ip_and_dns_generator.py
@@ -5,21 +5,29 @@ from nslookup import Nslookup
 current_directory = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_directory, 'top500Domains.csv')
 
+domains_unreachable = []
+
+
 df = pd.DataFrame(pd.read_csv(file_path, sep=','))
 
 dns_query = Nslookup()
 dns_query = Nslookup(dns_servers=["1.1.1.1"], verbose=False, tcp=False)
 
-print(df.head())
+# print(df.head()) # Debug para ver se o arquivo foi lido corretamente
 
-def ping_ip(dominio):
-    response = os.system("ping -c 10 " + dominio)
+def ping_ip(dominio): # ICMP
+    if dominio not in domains_unreachable:
+        response = os.system("ping -c 20 " + dominio)
+    else:
+        return False
+    
     if response == 0:
         return True
     else:
+        domains_unreachable.append(dominio)
         return False
 
-def nslookup_request(dominio):
+def nslookup_request(dominio): # DNS
     ips_record = dns_query.dns_lookup(dominio)
     print(ips_record.response_full, ips_record.answer)
     if ips_record.answer is not None:
@@ -29,20 +37,17 @@ def nslookup_request(dominio):
 
     
 def main():
-    os.system("sudo tcpdump -w segundo_ip.pcap -i eth0 icmp &")
+    os.system("sudo tcpdump -w terceiro_ping.pcap -i eth0 icmp &") # Iniciar o processo de captura icmp
+    # os.system("sudo tcpdump -w ajustes.pcap -i eth0 udp port 53 &") # Iniciar o processo de captura dns
     
     for index, row in df.iterrows():
-        if ping_ip(row['Root Domain']):
-        # if nslookup_request(row['Root Domain']):
+        if ping_ip(row['Root Domain']): # ICMP
+        # if nslookup_request(row['Root Domain']): # DNS
             print(row['Root Domain'])
         else:
             print('Não foi possível pingar/DNS o domínio: ' + row['Root Domain'])
-
-    
-    os.system("pid=$(ps -e | pgrep tcpdump)")
-    os.system("echo $pid")
-    os.system("sleep 5")
-    os.system("sudo kill -2 $pid")
+        
+    os.system("sudo pkill -2 tcpdump") # Matar o processo
     
 if __name__ == '__main__':
     main()
