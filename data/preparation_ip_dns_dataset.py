72781945dcdbff902cf28781b42ceb3e0050be41
Rafael Carneiro Pregardier <102990182+Pre9o@users.noreply.github.com>
2024-10-14 11:49:00 -0300

    Add files via upload

---
+++ data/preparation_ip_dns_dataset.py
@@ -0,0 +1,153 @@
+from imghdr import what
+import numpy as np
+import os
+import pyshark
+import argparse
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+pcap_dir = os.path.join(parent_dir, 'pcaps')
+
+npz_dir = os.path.join(current_dir, 'npz')
+
+if not os.path.exists(f"{npz_dir}"):
+    os.makedirs(f"{npz_dir}")
+
+
+def packet_useful_data(packet, start, end):
+    """Returns the useful data of a packet
+
+    Args:
+        packet (string): Packet
+
+    Returns:
+        string: Useful data of the packet
+    """    
+    return packet[start:end]
+
+def packet_means(packet):
+    """Returns the means of the bytes of a packet
+    
+    Args:
+        packet (string): Packet
+        
+    Returns:
+        list: List of means of the bytes of a packet
+    """
+    packet_list = []
+    packet_with_mean = []
+    
+    for i in range(0, len(packet), 2):
+        packet_list.append(packet[i:i+2])
+        
+    for i in range(0, 14):
+        packet_list.append('00')
+                
+    for i in range(0, len(packet_list)):
+        packet_with_mean.append(packet_list[i][:1] + '8')
+        packet_with_mean.append(packet_list[i][1:] + '8')
+        
+    return packet_with_mean
+
+def duplicate_and_map_bytes(byte_digits, n=28, d=2):
+    """Duplicates and maps the bytes of a packet
+    
+    Args:
+        byte_digits (list): List of bytes
+        n (integer): Size of the matrix
+        d (integer): Size of the submatrix
+        
+    Returns:
+        numpy.ndarray: Matrix of bytes
+    """
+    
+    # Initialize the n x n matrix with zeros of object type
+    matrix = np.zeros((n, n), dtype=object)
+
+    i, j = 0, 0
+    for byte_digit in byte_digits:
+        # Fill the d x d submatrix with the entered byte value (as string)
+        value = int(byte_digit, 16)  # Convert the hexadecimal string to integer
+        matrix[i:i+d, j:j+d] = value
+
+        # Update the indexes
+        j += d
+        if j >= n:
+            j = 0
+            i += d
+            if i >= n:
+                break
+
+    return matrix
+
+def what_protocol(packet):
+    """Returns the protocol of a packet
+    
+    Args:
+        packet (string): Packet
+        
+    Returns:
+        string: Protocol of the packet
+    """
+    if packet[18:20] == '11' and (packet[46:48] == '35' or packet[52:54] == '35'):
+        return 'DNS'
+    elif packet[18:20] == '01':
+        return 'ICMP'
+    elif packet[18:20] == '11':
+        return 'UDP'
+    else:
+        return 'Other'
+
+def main():
+    """Main function
+    """
+    parser = argparse.ArgumentParser(description="Process pcap file and generate dataset.")
+    parser.add_argument("pcap_name", type=str, nargs='?', default='default_pcap', help="Name of the pcap file")
+    parser.add_argument("packets_limit", type=int, nargs='?', default=20000, help="Number of packets to be processed")
+    parser.add_argument("useful_data_start", type=int, nargs='?', default=28, help="Start of the useful data, default is 28 (without Ethernet header)")
+    parser.add_argument("useful_data_end", type=int, nargs='?', default=300, help="End of the useful data, default is 300")
+    parser.add_argument("protocol", type=str, nargs='?', default='DNS', help="Protocol to be processed")
+    parser.add_argument("npz_file", type=str, nargs='?', default='default_npz', help="Name of the npz file")
+    
+    args = parser.parse_args()
+    
+    pcap_name = args.pcap_name
+    packets_limit = args.packets_limit
+    useful_data_start = args.useful_data_start
+    useful_data_end = args.useful_data_end
+    desired_protocol = args.protocol
+    
+    pcap_path = os.path.join(pcap_dir, pcap_name)
+    
+    dataset = {"x_train": [], "y_train": [], "x_test": [], "y_test": []}
+    
+    pcap = pyshark.FileCapture(pcap_path, use_json=True, include_raw=True)
+        
+    index = 0
+    
+    for pkt in pcap:
+        print(f"Packet {index + 1}")
+        
+        packet = pkt.frame_raw.value        
+        packet = packet_useful_data(packet, useful_data_start, useful_data_end)
+        protocol = what_protocol(packet)
+        
+        if protocol == desired_protocol:
+            
+            packet = packet_means(packet)
+            packet = duplicate_and_map_bytes(packet)
+            
+            dataset["x_train"].append(packet.tolist())
+                
+            index += 1
+            print (index)
+        if index == packets_limit:
+            break
+            
+    npz_file = npz_file
+            
+    np.savez(os.path.join(npz_dir, npz_file), **dataset)
+        
+if __name__ == "__main__":
+    main()
